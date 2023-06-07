52c12c6c6ac1379ad77df23faf86165d4d93052e
M. Nouman Ejaz <noumanejaz92@gmail.com>
2023-06-07 19:34:19 +0500

    Code Refactor and design update - Music Player

--- Minimilist Music Player/script.js
+++ Minimilist Music Player/script.js
@@ -5,62 +5,101 @@ const song = document.getElementById('song');
 const playIcon = document.querySelector('.play');
 const pauseIcon = document.querySelector('.pause');
 const thumbnail = document.querySelector('.song__img');
+const backImage = document.querySelector('.full-img');
+
 let val = 0;
-let newVal;
 let play = false;
 let rotation;
-// console.log(song.currentTime)
+
+// functions
+const shrinkThumbnail = function () {
+  thumbnail.style.width = '180px';
+  thumbnail.style.opacity = '.6';
+};
+const growThumbnail = function () {
+  thumbnail.style.width = '220px';
+  thumbnail.style.opacity = '.9';
+};
+const showPlayIcon = function () {
+  playIcon.classList.add('hidden');
+  pauseIcon.classList.remove('hidden');
+};
+const showPauseIcon = function () {
+  playIcon.classList.remove('hidden');
+  pauseIcon.classList.add('hidden');
+};
+
+
+// main working start on page load
 song.onloadedmetadata = function () {
+  shrinkThumbnail();
   //   song.pause();
   progress.max = song.duration;
   progress.value = song.currentTime;
 };
 
 const rotate = function () {
-  val += 2;
+  val++;
   thumbnail.style.transform = `rotate(${val}deg)`;
-  newVal = val;
-  if (val > 359) newVal = val = 0;
-  console.log(val);
+};
+
+const seek = function (play) {
+  if (play) {
+    setInterval(() => {
+      progress.value = song.currentTime;
+    }, 500);
+  }
 };
 
 const playPause = function () {
   if (!playIcon.classList.contains('hidden')) {
     song.play();
     play = true;
-    // // rotation
+    // rotation
+    clearInterval(rotation);
     if (play) {
       rotation = setInterval(rotate, 50);
-    } 
-    playIcon.classList.add('hidden');
-    pauseIcon.classList.remove('hidden');
+    }
+    // seek bar movement
+    seek(play);
+    growThumbnail();
+    showPlayIcon();
   } else if (playIcon.classList.contains('hidden')) {
     song.pause();
     play = false;
     clearInterval(rotation);
-    playIcon.classList.remove('hidden');
-    pauseIcon.classList.add('hidden');
+    shrinkThumbnail();
+    showPauseIcon();
   }
 };
-
-if (play) {
-  setInterval(() => {
-    progress.value = song.currentTime;
-  }, 500);
-}
-
 
 progress.onchange = function () {
   song.play();
   play = true;
-    // // rotation
-    clearInterval(rotation);
-    if (play) {
-      rotation = setInterval(rotate, 50);
-    } 
-  playIcon.classList.add('hidden');
-  pauseIcon.classList.remove('hidden');
+  growThumbnail();
   song.currentTime = progress.value;
+  // seek bar movement
+  seek(play);
+  // rotation
+  clearInterval(rotation);
+  if (play) {
+    rotation = setInterval(rotate, 25);
+  }
+  showPlayIcon();
 };
 
-// console.log(song.EndTime)
+thumbnail.addEventListener('click', changeThumbnail);
+
+// changing style when user click on the thumbnail
+function changeThumbnail() {
+  play = true;
+  backImage.classList.remove('hiddenAnim');
+  thumbnail.classList.add('hidden');
+}
+
+backImage.addEventListener('click', function () {
+  backImage.classList.add('hiddenAnim');
+  thumbnail.classList.remove('hidden');
+});
+
+console.log(song.length)
